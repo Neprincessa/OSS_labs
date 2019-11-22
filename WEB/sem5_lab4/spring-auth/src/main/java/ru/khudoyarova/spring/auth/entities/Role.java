d47ee648b39dfc8a67ffda04015975d5ac535ca5
Anastasia <asya-iriska@yandex.ru>
2019-11-23 01:04:32 +0300

    lab 4

---
+++ WEB/sem5_lab4/spring-auth/src/main/java/ru/khudoyarova/spring/auth/entities/Role.java
@@ -0,0 +1,15 @@
+package ru.khudoyarova.spring.auth.entities;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority
+{
+
+	USER;
+
+	@Override
+	public String getAuthority()
+	{
+		return name();
+	}
+}
